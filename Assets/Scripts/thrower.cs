bb3ee21ffb9a4f8e359de975228efdc19cfaee40
Swan Chase <16647@ma-web.nl>
2014-04-14 14:04:55 +0200

    added enemy

---
+++ Assets/Scripts/thrower.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class thrower : MonoBehaviour {
+	public float movementSpeed;
+	public float rotationSpeed;
+	
+	public Transform booger;
+	
+	// Update is called once per frame
+	void Update () {
+		float y = Input.GetAxis ("Horizontal");
+		float z = Input.GetAxis ("Vertical");
+		
+		transform.Translate (new Vector3 (0f, 0f, z) * movementSpeed * Time.deltaTime);
+		transform.Rotate(new Vector3 (0f, y, 0f) * rotationSpeed * Time.deltaTime);
+		
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			ShootBooger();
+		}
+	}
+	
+	void ShootBooger()
+	{
+		Instantiate (booger, this.transform.position, this.transform.rotation);
+	}
+}
---
+++ Assets/Scripts/weapon.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class weapon : MonoBehaviour {
+	public float DestroyTime;
+	public float speed;
+	
+	void Start () {
+		Destroy (gameObject, DestroyTime);
+	}
+	
+	void Update(){
+		this.transform.Translate (Vector3.forward * speed * Time.deltaTime);
+
+
+
+	} 
+	void OncolisionEnter(Collider other)
+	{
+
+	}
+}
