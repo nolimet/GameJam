9a686769ebabab7dc7a47b64904ae653a55747e7
Nolimet <nolimet@stamdata.nl>
2014-04-15 09:30:14 +0200

    sync

--- Assets/Scripts/thrower.cs
+++ Assets/Scripts/thrower.cs
@@ -1,27 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//[RequireComponent(typeof(Rigidbody))]
 public class thrower : MonoBehaviour {
-	public float movementSpeed;
-	public float rotationSpeed;
-	
-	public Transform weapon;
-	
+	public Transform booger;
+
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.WindowsWebPlayer || Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            Debug.Log("all is oke :D this is windows");
+        }
+        else
+        {
+            Debug.Log("notsure what this is D: I am removing my self");
+            Destroy(this);
+        }
+    }
 	// Update is called once per frame
-	void Update () {
-		float y = Input.GetAxis ("Mouse y");
-		float z = Input.GetAxis ("Mouse X");
-		
-		transform.Translate (new Vector3 (0f, 0f, z) * movementSpeed * Time.deltaTime);
-		transform.Rotate(new Vector3 (0f, y, 0f) * rotationSpeed * Time.deltaTime);
-		
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			ShootBooger();
-		}
-	}
-	
-	void ShootBooger()
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            RaycastHit2D hit = new RaycastHit2D();
+            hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null)
+            {
+                if (hit.collider != null)
+                {
+                    ShootBooger(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+
+	void ShootBooger(GameObject target)
 	{
-		Instantiate (weapon, this.transform.position, this.transform.rotation);
+
+        GameObject bullet = Instantiate(Resources.Load("Bullet"), transform.position, Quaternion.identity) as GameObject;
+       bullet.GetComponent<weapon>().getTarget(target.transform);
 	}
 }
