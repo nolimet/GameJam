693de7a01adc383af759662b12d6c36718c8d9e7
KingToadz <yorick_sk8@live.nl>
2014-04-16 14:25:01 +0200

    Updated global vars

--- Assets/Scripts/GlobalsVars.cs
+++ Assets/Scripts/GlobalsVars.cs
@@ -2,7 +2,12 @@
 using System.Collections;
 
 public class GlobalsVars : MonoBehaviour {
+	public static int Score = 0;
+	public static int currentWeapon = 0;
+	public static int trys = 3;
 
+	public GUIText score;
+	public GUIText tries;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +15,7 @@ public class GlobalsVars : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		score.text = "Score: " + Score;
+		tries.text = "Tries: " + tries;
 	}
 }
