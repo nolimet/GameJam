e6e485bfd7b72ca0fbd58e9448c59d08f3bb1746
Swan Chase <16647@ma-web.nl>
2014-04-16 14:28:00 +0200

    colision script

---
+++ Assets/Scripts/collision.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+
+	public Transform target;
+
+	void Update()
+	{
+
+	}
+
+	void OntriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") 
+		{
+			Debug.Log(other.tag);
+			target = other.transform;
+		}
+	}
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player") 
+		{
+			Debug.Log(other.tag);
+			target = null;
+		}
+	}
+	void OncolisionEnter(Collision other)
+	{
+		if (other.transform.tag == "Sphere1") 
+		{
+			Destroy(this.gameObject);
+			Destroy(other.gameObject);
+			Debug.Log ("blam boem bats");
+		}
+	}
+}
